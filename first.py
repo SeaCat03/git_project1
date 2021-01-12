ef0288e71ffaf5f5e5e858356feaeb537f69dc0c
SeaCat03 <yuliachen329@gmail.com>
2021-01-12 14:28:10 +0400

    Newhghffg

--- first.py
+++ first.py
@@ -1,8 +1,8 @@
-def print_hi(name):
+def print_hi():
     # Use a breakpoint in the code line below to debug your script.
     print('My first git-repo')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi()
